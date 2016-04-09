e05321975a7377426fa2075b2e8a7423e6daeffe
Jeremy <jermspeaks@gmail.com>
2016-04-09 15:23:19 -0700

    cleaned up data. Ready to show

---
+++ src/dataCleanup.js
@@ -0,0 +1,65 @@
+// Imports
+var originalDoc = require('../dist/medicare_pages.json');
+var fs = require('fs');
+
+// Initialize variables
+var transformedDoc;
+var newDoc;
+
+function writeOut(obj, file){
+  fs.writeFile(file, JSON.stringify(obj, null, 2), function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + file);
+    }
+  });
+}
+
+function mapSectionByPage(page) {
+  if (page < 15) {
+    return 'Introduction';
+  } else if (page >= 15 && page < 23) {
+    return 'Section 1- Learn How Medicare Works';
+  } else if (page >= 23 && page < 37) {
+    return 'Section 2—Signing Up for Medicare Part A and Part B';
+  } else if (page >= 37 && page < 75) {
+    return 'Section 3—Find Out if Medicare Covers Your Test, Service, or Item';
+  } else if (page >= 75 && page < 83) {
+    return 'Section 4—What’s Original Medicare?';
+  } else if (page >= 83 && page < 99) {
+    return 'Section 5—Learn about Medicare Advantage Plans (Part C) & Other Medicare Health Plans';
+  } else if (page >= 99 && page < 105) {
+    return 'Section 6—What are Medicare Supplement Insurance (Medigap) Policies?';
+  } else if (page >= 105 && page < 119) {
+    return 'Section 7— Get Information about Prescription Drug Coverage (Part D)';
+  } else if (page >= 119 && page < 127) {
+    return 'Section 8— Get Help Paying Your Health & Prescription Drug Costs';
+  } else if (page >= 127 && page < 141) {
+    return 'Section 9—Know Your Rights & Protect Yourself from Fraud';
+  } else {
+    return 'Section 10— Get More Information';
+  }
+}
+
+transformedDoc = originalDoc.map(function(entry, entryIndex) {
+  var newEntry = {};
+  if (entry.question.length === 0 && entry.section.length === 0) {
+    return null;
+  }
+
+  if (entry.section.length === 0) {
+    newEntry.section = mapSectionByPage(entry.page);
+  }
+
+  if (entry.question.length === 0) {
+    newEntry.question = entry.section;
+  }
+  return Object.assign({}, entry, newEntry);
+});
+
+newDoc = transformedDoc.filter(function(entry) {
+  return entry !== null;
+});
+
+writeOut(newDoc, "./dist/medicare_pages_parsed.json");
--- src/parser.js
+++ src/parser.js
@@ -13,46 +13,114 @@ function QuestionCreator(collection) {
   }
 }
 
+function createNewQuestion(constructor) {
+  return Object.assign({}, {
+    question: "",
+    paragraph: "",
+    section: "",
+    page: null
+  }, constructor);
+}
+
 var questionCreator = QuestionCreator(newDocumentJson);
 
+// Loop through each page
 jsonDocument.formImage.Pages.forEach(function(page, pageIndex) {
   var pageText = page.Texts;
-  var question = "";
-  var paragraph = "";
-  var questionSection = "";
-  var extraInformation = "";
-  var captureEndOfPage = false;
+
+  // Initial Capture Mode must be on to work
+  var captureMode = false;
+
+  // Logic gates for which state of capture mode we are currently in
+  var captureSection = false;
+  var captureQuestion = false;
+  var captureParagraph = false;
+
+  var capturedSection = "";
+
+  var currentQuestion;
+  // Loop through the text of each page
   pageText.forEach(function(text, textIndex) {
+    // Find where it is styled with a blue box around the text
     if (text.sw === 0.26953125) {
-      var textSection = text.R;
-      textSection.forEach(function(section) {
-        // console.log(section.TS, decodeURIComponent(section.T));
-        if (section.TS[1] === 18) {
-          questionSection += decodeURIComponent(section.T);
-        } else if (section.TS[1] === 22) {
-          question += decodeURIComponent(section.T);
-        } else {
-          extraInformation += decodeURIComponent(section.T);
+      if (captureMode && captureParagraph) {
+        captureSection = true;
+        captureQuestion = false;
+        captureParagraph = false;
+
+        // Push question to array
+        if (!currentQuestion.page) {
+          currentQuestion.page === pageIndex;
         }
+        questionCreator(currentQuestion);
+
+        // Reset new question
+        currentQuestion = createNewQuestion({
+          page: pageIndex
+        });
+      }
+
+      var textSection = text.R;
+      // Loop through the section of each page
+      textSection.forEach(function(section) {
+        if (captureMode) {
+          if (section.TS[1] === 18 && captureSection) {
+            currentQuestion.section += decodeURIComponent(section.T);
+          } else if (section.TS[1] === 22 && captureSection) {
+            // Set captured question to new section,
+            // else set it to the previous
+            if (capturedSection.length > 0) {
+              capturedSection = currentQuestion.section;
+            } else {
+              currentQuestion.section = capturedSection;
+            }
+            captureSection = false;
+            captureQuestion = true;
+
+            currentQuestion.question += decodeURIComponent(section.T);
+          } else if (section.TS[1] === 22 && captureQuestion) {
+            currentQuestion.question += decodeURIComponent(section.T);
+          } else if ((section.TS[1] !== 18 || section.TS[1] !== 22) && (captureSection || captureQuestion)) {
+            captureSection = false;
+            captureQuestion = false;
+            captureParagraph = true;
+            currentQuestion.paragraph += decodeURIComponent(section.T);
+          } else if ((section.TS[1] !== 18 || section.TS[1] !== 22) && captureParagraph) {
+            currentQuestion.paragraph += decodeURIComponent(section.T);
+          }
+        } else if (section.TS[1] === 18) {
+          captureMode = true;
+          captureSection = true;
+          currentQuestion = createNewQuestion({
+            page: pageIndex
+          });
+          currentQuestion.section += decodeURIComponent(section.T);
+        }
+
+        // if (section.TS[1] === 18) {
+        //   questionSection += decodeURIComponent(section.T);
+        // } else if (section.TS[1] === 22) {
+        //   question += decodeURIComponent(section.T);
+        // } else {
+        //   extraInformation += decodeURIComponent(section.T);
+        // }
       });
-      captureEndOfPage = true;
-    } else if (captureEndOfPage) {
+    } else if (captureMode && !captureParagraph) {
+      captureSection = false;
+      captureQuestion = false;
+      captureParagraph = true;
+
       var textSection = text.R;
       textSection.forEach(function(section) {
-        paragraph += decodeURIComponent(section.T);
+        currentQuestion.paragraph += decodeURIComponent(section.T);
+      });
+    } else if (captureMode && captureParagraph) {
+      var textSection = text.R;
+      textSection.forEach(function(section) {
+        currentQuestion.paragraph += decodeURIComponent(section.T);
       });
     }
   });
-
-  if (question.length > 0) {
-    questionCreator({
-      question: question,
-      section: questionSection,
-      extraInformation: extraInformation,
-      paragraph: paragraph,
-      page: pageIndex
-    });
-  }
 });
 
 function unicodeToChar(text) {
@@ -60,6 +128,8 @@ function unicodeToChar(text) {
     return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
   });
 }
+
+// console.log(JSON.stringify(newDocumentJson, null, 2));
 
 function writeOut(obj){
   fs.writeFile(outputFilename, JSON.stringify(obj, null, 2), function(err) {
