bb4ec6891dd48295db416f70419bbdb1b5997d78
Jeremy <jermspeaks@gmail.com>
2016-04-10 14:28:25 -0700

    created final list of questions

--- src/reader.js
+++ src/reader.js
@@ -1,5 +1,20 @@
 var originalDoc = require('../dist/medicare_pages.json');
+var fs = require('fs');
 
-originalDoc.forEach(function(entry) {
-  console.log(entry.question);
-});
+function exportQuestions() {
+  var writeStream = fs.createWriteStream('./dist/questions.txt');
+
+  originalDoc.forEach(function(entry) {
+    writeStream.write(entry.question + "\n");
+  });
+
+  writeStream.end();
+}
+
+if (process.argv[2] === '--export') {
+  switch (process.argv[3]) {
+  case 'questions':
+  default:
+    exportQuestions();
+  }
+}
