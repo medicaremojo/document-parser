6f54ebebe615a20b60a2e9b23ad4fda40693b15d
Jeremy <jermspeaks@gmail.com>
2016-04-14 21:24:20 -0700

    categories created

--- src/category_ranker.js
+++ src/category_ranker.js
@@ -1,5 +1,29 @@
 var categoryObj = require('../dist/categories.json');
 
+var rank = {};
+var rankedList = [];
+
 categoryObj.forEach(function(entry) {
-  console.log(entry.categories);
+  entry.categories.forEach(function(category) {
+    if (rank.hasOwnProperty(category)) {
+      rank[category]++;
+    } else {
+      rank[category] = 1;
+    }
+  })
 });
+
+for (key in rank) {
+  if (rank.hasOwnProperty(key)) {
+    rankedList.push({
+      category: key,
+      count: rank[key]
+    });
+  }
+}
+
+var sorted = rankedList.sort(function(a, b) {
+    return a.count - b.count;
+});
+
+console.log(sorted);
